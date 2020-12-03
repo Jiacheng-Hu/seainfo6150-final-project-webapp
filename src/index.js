40ca9bbe2426d0975ef5639608a895a64635f6ce
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2020-12-03 14:42:10 +0800

    final-project

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
-// import LoginApp from "./Login/LoginApp";
+import LoginApp from "./Login/LoginApp";
 import Project from "./Project"
-import ErrorPage from "./ErrorPage/ErrorPage"
+// import ErrorPage from "./ErrorPage/ErrorPage"
 
 ReactDOM.render(
   
   <Router basename={/github.io/.test(window.location) ? "/seainfo6150-final-project-webapp" : ""}>
     <Switch>
-      <Route path="/demo" exact>
+      {/* <Route path="/demo" exact>
         <App/>
-      </Route>
-      <Route path="/" exact>
-        <Project/>
-      </Route>
-      {/* <Route path="/homepage" exact>
-        <Project/>
       </Route> */}
-      <Route>
-        <ErrorPage/>
+      {/* <Route path="/" exact>
+        <LoginApp/>
+      </Route> */}
+      <Route path="/">
+        <Project/>
       </Route>
+      {/* <Route>
+        <ErrorPage/>
+      </Route> */}
     </Switch>
   </Router>,
   document.getElementById("root")
