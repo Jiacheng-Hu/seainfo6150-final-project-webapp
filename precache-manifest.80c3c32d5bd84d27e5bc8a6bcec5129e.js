0eee11174e4ce285560fbf5fd81f553faa319cc2
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2020-12-03 14:45:19 +0800

    Updates

--- precache-manifest.80c3c32d5bd84d27e5bc8a6bcec5129e.js
+++ precache-manifest.80c3c32d5bd84d27e5bc8a6bcec5129e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a68301311692680802ae914496e340f",
+    "revision": "e71f1f18d89161fcf5887903f1718738",
     "url": "/seainfo6150-final-project-webapp/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/seainfo6150-final-project-webapp/static/js/2.89cf8298.chunk.js"
   },
   {
-    "revision": "679086ee3adef8561c75",
-    "url": "/seainfo6150-final-project-webapp/static/js/main.840ae555.chunk.js"
+    "revision": "7cb371ff89758ff953cc",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.72f38df9.chunk.js"
   },
   {
     "revision": "940ec4a42937c37ae625",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.4fd4df5e0fb0f9318b8418deafbb869e.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.80c3c32d5bd84d27e5bc8a6bcec5129e.js"
 );
 
 self.addEventListener('message', (event) => {
